fa87dd613ddf5f19383f58fd1c3dc5d4debe9336
IvanJevtic9 <ijevtic459@gmail.com>
2023-01-15 11:50:21 +0100

    Record types commit

---
+++ DotNetTopics/RecordTypes/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace RecordTypes.Models
+{
+    //public record Person
+    //{
+    //    public string FullName { get; init; }
+    //    public DateOnly DateOfBirth { get; init; }
+    //}
+    
+    public record struct PersonAsStruct(string FullName, DateTime DateTime);
+
+    public record Person(string FullName, DateOnly DateOfBirth);
+}
---
+++ DotNetTopics/RecordTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿using RecordTypes.Models;
+
+var person = new Person("Ivan Jevtic", new DateOnly(1995, 10, 16)); // properties that we define here is immutable
+var person2 = person with
+{
+    FullName = "Branislav Jevtic"
+};
+
+var (name, dateOfBirth) = person; // name = Ivan Jevtic dateOfBirth = 10/16/1995
+
+
+Console.WriteLine(person); // Person { FullName = Ivan Jevtic, DateOfBirth = 10/16/1995 }
+Console.WriteLine(person == person2); // False
